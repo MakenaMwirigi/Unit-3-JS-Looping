47c292e2fe9bd6e711f39a4b2880a155f1b2775c
MakenaMwirigi <makenasharon46@gmail.com>
2025-05-13 12:33:19 +0300

    looping assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function writeCards(names, eventName) {
+  const messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+
+  return messages;
+}
+
+// Function to count down from a given number to 0
+function countDown(startingNumber) {
+  while (startingNumber >= 0) {
+    console.log(startingNumber);
+    startingNumber--;
+  }
+}
